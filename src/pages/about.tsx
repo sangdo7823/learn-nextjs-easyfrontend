345ef47f75d71f22bbb60edde475ec535834036b
Sang Do <sangdo.eng@gmail.com>
2023-04-09 16:16:21 +0700

    proxy

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,28 +1,29 @@
 // import Header from '@/components/common/header';
-import dynamic from 'next/dynamic';
-import { useRouter } from 'next/router';
-import React from 'react';
-const Header = dynamic(()=> import('@/components/common/header'),{ssr:false})
-export interface AboutPageProps {
+import { MainLayout } from '@/components/layout'
+import { NextPageWithLayout } from '@/models'
+import dynamic from 'next/dynamic'
+import { useRouter } from 'next/router'
+import React from 'react'
+const Header = dynamic(() => import('@/components/common/header'), { ssr: false })
+export interface AboutPageProps {}
+const About: NextPageWithLayout = (props: AboutPageProps) => {
+	const router = useRouter()
+	function goToProduct() {
+		router.push({
+			pathname: '/product/[productId]',
+			query: {
+				productId: 123,
+				ref: 'social',
+			},
+		})
+	}
+	return (
+		<>
+			<h1>About Page</h1>
+			<button onClick={goToProduct}>Go to Product</button>
+			<Header />
+		</>
+	)
 }
-
-export default function App (props: AboutPageProps) {
-    const router = useRouter();
-    function goToProduct(){
-        router.push({
-            pathname: '/product/[productId]',
-            query: {
-                productId: 123,
-                ref: 'social',
-            },
-            })
-    }
-  return (
-    <div>
-      <h1>About Page</h1>
-      <button onClick={goToProduct}>Go to Product</button>
-      <Header/>
-    </div>
-  );
-}
- 
+About.Layout = MainLayout
+export default About
